e9cb492e5e53e068b900a518ceb03e188aa05efd
martin-banks <anotherbanksy@gmail.com>
2019-08-29 23:33:56 +1000

    updates layout names

--- src/config/layouts.js
+++ src/config/layouts.js
@@ -1,17 +1,17 @@
 export default {
-  '16 x 9': {
+  '16x9': {
     card: {
       width: 1920,
       height: 1080,
     },
   },
-  '4 x 3': {
+  '4x3': {
     card: {
       width: 1920,
       height: 1440,
     }
   },
-  '3 x 4': {
+  '3x4': {
     card: {
       width: 1440,
       height: 1920,
