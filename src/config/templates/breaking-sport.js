11581bab18b01c6d5fbeb023b211db9ae07abf8a
martin-banks <anotherbanksy@gmail.com>
2019-09-04 17:22:17 +1000

    starts setup for breaking sport layout

--- src/config/templates/breaking-sport.js
+++ src/config/templates/breaking-sport.js
@@ -1,5 +1,9 @@
 import preview from './previews/placeholder.jpg'
 
+import background16x9 from '../images/backgrounds/breaking-sport/16x9'
+import backgroundInstagramStories from '../images/backgrounds/breaking-sport/instagram-stories'
+import backgroundsquare from '../images/backgrounds/breaking-sport/square'
+
 export default {
   info: {
     title: 'Breaking: Sport',
@@ -8,10 +12,34 @@ export default {
   layerItems: {
     '16x9': [
       {
+        name: 'backgroundImageFixed',
+        settings: {
+          label: 'background-image',
+          src: background16x9,
+          x: 0,
+          y: 0,
+          width: '100%',
+          height: '100%',
+        }
+      },
+      {
+        name: 'croppedImage',
+        settings: {
+          label: 'display-image',
+          x: '4.5%',
+          y: '17%',
+          width: '39%',
+          height: '74.6%',
+          opacity: 1
+        },
+      },
+      {
         name: 'logoSportTelegraph',
         settings: {
           label: 'logo-sport-telegraph',
-          // settings in here
+          x: '3%',
+          y: '5.5%',
+          width: '14%',
         }
       },
     ],
@@ -33,23 +61,65 @@ export default {
     //     }
     //   },
     // ],
-    // 'square': [
-    //   {
-    //     name: 'Layer name',
-    //     settings: {
-    //       label: 'layer-name',
-    //       // settings in here
-    //     }
-    //   },
-    // ],
-    // 'Instagram stories': [
-    //   {
-    //     name: 'Layer name',
-    //     settings: {
-    //       label: 'layer-name',
-    //       // settings in here
-    //     }
-    //   },
-    // ],
+    'square': [
+      {
+        name: 'overlayImage',
+        settings: {
+          label: 'background-image',
+          src: backgroundsquare,
+          x: 0,
+          y: 0,
+          width: '100%',
+          height: '100%',
+        }
+      },
+      {
+        name: 'croppedImage',
+        settings: {
+          label: 'display-image',
+          x: '5%',
+          y: '5%',
+          width: '50%',
+          height: '40%',
+        },
+      },
+      {
+        name: 'logoSportTelegraph',
+        settings: {
+          label: 'logo-sport-telegraph',
+          // settings in here
+        }
+      },
+    ],
+    'Instagram stories': [
+      {
+        name: 'overlayImage',
+        settings: {
+          label: 'background-image',
+          src: backgroundInstagramStories,
+          x: 0,
+          y: 0,
+          width: '100%',
+          height: '100%',
+        }
+      },
+      {
+        name: 'croppedImage',
+        settings: {
+          label: 'display-image',
+          x: '5%',
+          y: '5%',
+          width: '50%',
+          height: '40%',
+        },
+      },
+      {
+        name: 'logoSportTelegraph',
+        settings: {
+          label: 'logo-sport-telegraph',
+          // settings in here
+        }
+      },
+    ],
   },
 }
