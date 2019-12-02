353378439f79f4c652484b562e00b25e7e92b6ee
martin-banks <anotherbanksy@gmail.com>
2019-12-02 13:23:28 +1100

    adds demo build of cardkit

---
+++ cardkit/precache-manifest.7b4a631f53dbf18768ba118b59ac73c1.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a622dc28fbc00f5d4d4a7a0f696edebb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d573ea4312dca6b765b",
+    "url": "/static/css/main.efa35478.chunk.css"
+  },
+  {
+    "revision": "6b02cb9032fc3461d947",
+    "url": "/static/js/2.c99f9b0a.chunk.js"
+  },
+  {
+    "revision": "1d573ea4312dca6b765b",
+    "url": "/static/js/main.82f035c2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "21cba2f87839dfca15342f719e2af84a",
+    "url": "/static/media/16x9.21cba2f8.jpg"
+  },
+  {
+    "revision": "276309efa6f5ea6af1d27a1d085314c4",
+    "url": "/static/media/16x9.276309ef.jpg"
+  },
+  {
+    "revision": "0cf5e2e979be14c33463e997b6bd0f68",
+    "url": "/static/media/3x4.0cf5e2e9.jpg"
+  },
+  {
+    "revision": "23a1020b2cd3d011ef11a9e6f05db7ba",
+    "url": "/static/media/3x4.23a1020b.jpg"
+  },
+  {
+    "revision": "4347725008f7b17a8a53d16c1c65e578",
+    "url": "/static/media/3x4.43477250.jpg"
+  },
+  {
+    "revision": "9fd1a6e406a8fd7c5de6e953364d09f8",
+    "url": "/static/media/3x4.9fd1a6e4.jpg"
+  },
+  {
+    "revision": "34c086904833de815c36beb241b408e5",
+    "url": "/static/media/Instagram stories.34c08690.jpg"
+  },
+  {
+    "revision": "43e966d3f76486b21c925a56ac06c57f",
+    "url": "/static/media/Instagram stories.43e966d3.jpg"
+  },
+  {
+    "revision": "7856e9a5bdc8f995d683570ac7280e1b",
+    "url": "/static/media/Instagram stories.7856e9a5.jpg"
+  },
+  {
+    "revision": "8a2d3c80f946fec2a754837b1d63d219",
+    "url": "/static/media/Instagram stories.8a2d3c80.jpg"
+  },
+  {
+    "revision": "a958d10641f2bc8bf4e592c07aece35b",
+    "url": "/static/media/Instagram stories.a958d106.jpg"
+  },
+  {
+    "revision": "c9fbab047a5c3e5c28a484581b7f9c5a",
+    "url": "/static/media/Instagram stories.c9fbab04.jpg"
+  },
+  {
+    "revision": "47683e1d2327ff2a6f847868282e8feb",
+    "url": "/static/media/breaking-news.47683e1d.jpg"
+  },
+  {
+    "revision": "bd5e167ad448398f62ead2a6f615ee6d",
+    "url": "/static/media/breaking-sport.bd5e167a.jpg"
+  },
+  {
+    "revision": "c1d2274b9460b3d9e9fbdd2a6628db32",
+    "url": "/static/media/display-quote.c1d2274b.jpg"
+  },
+  {
+    "revision": "6617734393463c206336f67d7488a5f2",
+    "url": "/static/media/inset-single.66177343.jpg"
+  },
+  {
+    "revision": "ba7d2d4a44d23771f759376571f02923",
+    "url": "/static/media/placeholder.ba7d2d4a.jpg"
+  },
+  {
+    "revision": "337b8b72e77eab951a96379dd47bb92c",
+    "url": "/static/media/square.337b8b72.jpg"
+  },
+  {
+    "revision": "9d46fe896b9a99ee729337a14450ebff",
+    "url": "/static/media/square.9d46fe89.jpg"
+  },
+  {
+    "revision": "cbc360724435719d955620561c64e084",
+    "url": "/static/media/text-overlay.cbc36072.jpg"
+  }
+]);
